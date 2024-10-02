9b10d2b240db69b19bd824046c89c320780aaf1b
paull <paullopisparrilla@gmail.com>
2024-10-02 10:18:44 +0100

    Final Person commit

--- src/software/ulpgc/Person.java
+++ src/software/ulpgc/Person.java
@@ -1,4 +1,40 @@
 package software.ulpgc;
 
+import java.time.LocalDate;
+
 public class Person {
+    private final String name;
+
+    private final LocalDate birthday;
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public Person(String name, LocalDate birthday) {
+
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", birthday=" + birthday +
+                ", age=" + getAge() +
+                '}';
+    }
+
+    private int getAge() {
+        return toYears(LocalDate.now().toEpochDay() - birthday.toEpochDay());
+    }
+
+    private int toYears(long days) {
+        return (int) (days/365);
+    }
 }
