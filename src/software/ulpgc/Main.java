d1e60e2184809e46f49582414d497870d9041a29
paull <paullopisparrilla@gmail.com>
2024-10-02 10:19:34 +0100

    Final Main commit

--- src/software/ulpgc/Main.java
+++ src/software/ulpgc/Main.java
@@ -1,4 +1,10 @@
 package software.ulpgc;
 
+import java.time.LocalDate;
+
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(new Person("Pablo", LocalDate.of(2004, 7, 8)));
+    }
 }
